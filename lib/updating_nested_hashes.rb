0f8835cab45f201a9bc259ab468d828b4b73ec75
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-31 02:39:54 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -65,7 +65,7 @@ def add_characters
    epic_tragedy[:additional_characters] = [
      
       { :name => "Prince Escalus" },
-      { :name => "Apothecary"
+      { :name => "Apothecary"}
      
      ]
 
